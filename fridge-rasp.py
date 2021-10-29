1098fe44343098ea782b885116f96bdc4804e68c
Tomek Karwowski <to.karwowski@gmail.com>
2021-10-29 16:34:31 +0200

    dht should work

--- fridge-rasp.py
+++ fridge-rasp.py
@@ -11,13 +11,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 # read data using pin 14
-instance = dht11.DHT11(pin = 14)
+dht = dht11.DHT11(pin = 21)
 
 temp_list = [0 for _ in range(10)]
 
 def run():
     while True:
-        result = instance.read()
+        result = dht.read()
         
         if result.is_valid():
             print("Temperature: %-3.1f C" % result.temperature)
@@ -26,7 +26,9 @@ def run():
             if result.temperature > all(temp_list, lambda x: x > MAX_TEMP):
                 alert(result)
                
-            temp_list
+            temp_list.append(result)
+            temp_list.pop(0)
+            print(temp_list)
             time.sleep(60)
         else:
             print("Error: %d" % result.error_code)
