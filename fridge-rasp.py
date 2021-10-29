e51f49990d003c81e1434baf2a44578b3852def8
Tomek Karwowski <to.karwowski@gmail.com>
2021-10-29 16:11:47 +0200

    initial commit

---
+++ fridge-rasp.py
@@ -0,0 +1,35 @@
+import time
+
+import RPi.GPIO as GPIO
+import dht11
+
+MAX_TEMP = 10
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin = 14)
+
+temp_list = [0 for _ in range(10)]
+
+def run():
+    while True:
+        result = instance.read()
+        
+        if result.is_valid():
+            print("Temperature: %-3.1f C" % result.temperature)
+            print("Humidity: %-3.1f %%" % result.humidity)
+    
+            if result.temperature > all(temp_list, lambda x: x > MAX_TEMP):
+                alert(result)
+               
+            temp_list
+            time.sleep(60)
+        else:
+            print("Error: %d" % result.error_code)
+    
+if __name__ == "__main__":
+    run()
